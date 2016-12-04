42d4786176e8da926d14a74ce915e654859937c6
Marco Zanchi <Marco Zanchi>
2016-12-04 20:13:27 -0200

    initial commit

---
+++ src/init.go
@@ -0,0 +1,29 @@
+package hello
+
+import (
+    "fmt"
+    "time"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func eccles() string{
+	today := time.Now().Weekday()
+	switch time.Friday {
+	case today + 0:
+		return "Today."
+	case today + 1:
+		return "Tomorrow."
+	case today + 2:
+		return "In two days."
+	default:
+		return "Too far away."
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "When's Friday?")
+    fmt.Fprint(w, eccles())
+}
